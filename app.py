cd2db47e3c1cb9063230a7bef64f8ebca18d1527
demidraksin <greatraksin@icloud.com>
2022-05-21 18:50:37 +0300

    Adding shortener function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, flash
+from hashids import Hashids
 
 
 def get_db_connection():
@@ -9,6 +10,7 @@ def get_db_connection():
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'try-to-guess'
+hashid = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,6 +21,14 @@ def index():
         if not url:  # если поле передали пустым
             flash('The URL is required!')
             return redirect(url_for('index'))
+        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)', (url,))
+        conn.commit()
+        conn.close()
+
+        url_id = url_data.lastrowid  # записываю id объекта из БД
+        hash_url = hashid.encode(url_id)  # хеширую id урла
+        short_url = request.host_url + hash_url
+        return render_template('index.html', s_url=short_url)
     return render_template('index.html')
 
 if __name__ == '__main__':
