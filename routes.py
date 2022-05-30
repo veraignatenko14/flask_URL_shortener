597d87f47493ac5b6e74face70fc2a4dc360284e
demidraksin <greatraksin@icloud.com>
2022-05-30 08:54:37 +0300

    Finishing refactoring

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from app import app, hashid, login
+import hashlib
+from app import app, hashid, login, db
 from flask import render_template, request, redirect, url_for, flash
 from models import User, Url
 
@@ -10,18 +11,18 @@ def load_user(id):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    conn = get_db_connection()  # подключаемся к БД
     if request.method == 'POST':  # если форма отправляется
         url = request.form['url']  # сохраняю урл из формы в переменную
         if not url:  # если поле передали пустым
             flash('The URL is required!')
             return redirect(url_for('index'))
-        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)', (url,))
-        conn.commit()
-        conn.close()
-
-        url_id = url_data.lastrowid  # записываю id объекта из БД
-        hash_url = hashid.encode(url_id)  # хеширую id урла
-        short_url = request.host_url + hash_url
-        return render_template('index.html', s_url=short_url)
+        hash_link = hashlib.sha256(bytes(url, 'ascii'))
+        new_url = Url(
+            original_url=url,
+            short_url=hash_link.hexdigest(),
+            user_id=1
+        )
+        db.session.add(new_url)
+        db.session.commit()
+        return render_template('index.html')
     return render_template('index.html')
