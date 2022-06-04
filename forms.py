4887d4ef0679def53c6ebc159fbb53f8fa07c6a8
demidraksin <greatraksin@icloud.com>
2022-06-04 15:07:38 +0300

    Adding login+register functions

---
+++ forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, EqualTo
+from models import User
+
+
+class LoginForm(FlaskForm):  # пишу свою форму поверх базовой формы из фласка
+    username = StringField('Имя пользователя: ')
+    password = PasswordField('Пароль: ')
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):  # форма регистрации на оснвое базовой формы из Flask
+    username = StringField('Имя пользователя: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[DataRequired()])
+    password = PasswordField('Пароль: ', validators=[DataRequired()])
+    password_again = PasswordField('Пароль (подтверждение): ',
+                                   validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def check_username(self, username):
+        # найду пользователя в Базе Данных
+        user = User.query.filter_by(username=username.data)
+        if user is not None:  # если пользователь есть в БД
+            raise ValidationError('Пользователь с таким ником уже зарегистрирован!')
+
+    def check_email(self, email):
+        # найду пользователя в Базе Данных
+        user = User.query.filter_by(email=email.data)
+        if user is not None:  # если пользователь есть в БД
+            raise ValidationError('Пользователь с такой почтой уже зарегистрирован!')
--- routes.py
+++ routes.py
@@ -2,6 +2,8 @@ import hashlib
 from app import app, hashid, login, db
 from flask import render_template, request, redirect, url_for, flash
 from models import User, Url
+from flask_login import current_user, login_user, logout_user, login_required
+from forms import LoginForm, RegistrationForm
 
 
 @login.user_loader
@@ -26,3 +28,48 @@ def index():
         db.session.commit()
         return render_template('index.html')
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():  # если форма отправляется
+        user = User.query.filter_by(username=form.username.data).first()  # пытаюсь найти пользователя в БД по логину
+        if user is None or not user.check_password(form.password.data):
+            # если пользователь не найден в БД или пароль не совпал
+            return redirect(url_for('login'))  # вернуть пользователя на страницу входа
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Login page', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main_page'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:  # если пользователь вошел
+        return redirect(url_for('index'))  # перенаправим на главную
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)  # добавить пользователя в БД
+        db.session.commit()  # сохранить пользователя в БД
+        return redirect(url_for('login'))  # перенаправить на страницу входа
+    return render_template('register.html', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
